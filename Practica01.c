a568faff7065967c8ff9c1c43562dcd5f2b07bcf
ColinTony <colincitoheredia@gmail.com>
2018-03-05 20:19:25 -0600

    IniciandoPractica

---
+++ Practica01.c
@@ -0,0 +1,18 @@
+/*
+PRACTICA 01
+AUTORES: Colin Heredia Luis Antonio 
+VERSIÓN: 1.0
+
+DESCRIPCIÓN: Con la implementación del TAD Pila en C, (estática y dinámica)
+implementamos un programa que valide y evalué una expresión infija. Esta expresion
+se pasara a posfija para poder evaluarla con valores ingresados por el usuario.
+
+OBSERVACIONES: Utilizamos una Estructura de datos dinamica , la cual
+nos ayudara para el algoritmo de conversion de infija a posfija, la validacion
+de la expresion y la evaluacion de la expresion con los valores deseados.
+
+COMPILACIÓN : usamos el comando gcc Practica01.c TADPila.c -o Practica01.c
+*/
+
+
+
